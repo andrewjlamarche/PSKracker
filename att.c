ea94437de1e68517777debebfd0b5bc704a9e7ea
soxrok2212 <soxrok2212@gmail.com>
2017-07-13 21:01:48 -0400

    update att.c

--- att.c
+++ att.c
@@ -2,33 +2,80 @@
  * att.c
  *
  *  Created on: Jul 11, 2017
- *      Author: soxrok2212
+ *      Author: soxrok2212 and rofl0r
  */
 
 #include <stdio.h>
+#include <getopt.h>
+#include <stdlib.h>
 #include <limits.h>
+#include <string.h>
 
-void genpass(unsigned x, unsigned char *buf) {
+#include "att.h"
+#include "version.h"
+
+int DONE = 0;
+
+static const char *option_string = "m:h";
+static const struct option long_options[] = { { "mode", required_argument, 0,
+		'm' }, { "help", no_argument, 0, 0 }, { 0, no_argument, 0, 'h' }, { 0,
+		0, 0, 0 } };
+
+void genpass589(unsigned x, unsigned char *buf1) {
 	static const char CHARSET[] = "abcdefghijkmnpqrstuvwxyz23456789#%+=?";
 	long double x1 = x * 465661287.5245797;
 	long long x2 = x1;
 	int i;
-	buf[12] = 0;
+	buf1[12] = 0;
 	for (i = 0; i < 6; i++) {
 		int k1 = CHARSET[x2 % 37];
 		x2 /= 37;
 		int k2 = 50 + (x2 % 8);
 		x2 /= 37;
-		buf[(10 - (i * 2)) + 1] = k1;
-		buf[(10 - (i * 2)) + 0] = k2;
+		buf1[(10 - (i * 2)) + 1] = k1;
+		buf1[(10 - (i * 2)) + 0] = k2;
 	}
 }
 
-char main() {
-	static const char VERSION[] = "0.1";
-	unsigned char pw[12];
-	for (unsigned j = 0; j <= INT_MAX; j++) {
-		genpass(j, pw);
-		printf("%s\n", pw);
+void genpass599(unsigned y, unsigned char *buf2) {
+	// not implemented yet
+}
+
+int main(int argc, char **argv) {
+	unsigned char pw[13];
+	int opt = 0;
+	int long_index = 0;
+
+	opt = getopt_long(argc, argv, option_string, long_options, &long_index);
+	while (opt != -1) {
+		switch (opt) {
+		case 'm':
+			if ((strcmp("nvg589", optarg)) == 0) {
+				for (unsigned j = 0; j <= 10; j++) {
+					genpass589(j, pw);
+					printf("%s\n", pw);
+				}
+				goto eof;
+			} else if ((strcmp("nvg599", optarg)) == 0) {
+				/* for (unsigned j = 0; j <= INT_MAX; j++) {
+				 genpass599(j, pw);
+				 printf("%s\n", pw);
+				 }
+				 goto eof;
+				 */
+			} else {
+				goto usage_err;
+			}
+			break;
+		case 'h':
+			goto usage_err;
+			break;
+		default:
+			break;
+		}
+		opt = getopt_long(argc, argv, option_string, long_options, &long_index);
 	}
+	usage_err: fprintf(stderr, usage, SHORT_VERSION);
+	eof: ;
+	return DONE;
 }
---
+++ att.h
@@ -0,0 +1,20 @@
+#ifndef ATT_H_
+#define ATT_H_
+char usage[] =
+	"\n"
+	"PSKracker WiFi Security Auditing Toolkit\n"
+	"ATTxxxxxxx WPA2 Bruteforce Utility %s\n"
+	"Copyright (c) 2017, soxrok2212 <soxrok2212@gmail.com>\n"
+	"\n"
+	"Usage: att <arguments>\n"
+	"\n"
+	"Required Arguments:\n"
+	"\n"
+	"	-m, --mode        : Target model nvg89 or nvg599\n"
+	"\n"
+	"Example:\n"
+	"\n"
+	"	att -m nvg589\n"
+	"\n";
+
+#endif /* ATT_H_ */
