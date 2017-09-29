98312e5e81a2c65489c8a51f7dd8bb283ea58585
soxrok2212 <soxrok2212@gmail.com>
2017-09-29 11:28:30 -0400

    add keyspace list

--- src/att.c
+++ src/att.c
@@ -3,7 +3,43 @@
  */
 #include <math.h>
 #include "att.h"
+#include <pthread.h>
+#define NUM_THREADS 5
 
+void *mt589(void *threadid, unsignedx, unsigned char *buf) {
+	long tid;
+	tid = (long) threadid;
+	
+	static const char CHARSET[] = "abcdefghijkmnpqrstuvwxyz23456789#%+=?";
+	long long x1 = x * 465661287.5245797; // thank you mrfancypants for finding this number
+        long long x2 = x1;
+        int i;
+        buf[12] = 0; // create buffer for password
+
+        /**
+         * This code finds the associated ascii value in the charset specified above
+         * If we knew the seed they were using, we wouldn't have to bruteforce every
+         * possibility
+         */
+        for (i = 0; i < 6; i++) {
+                int k1 = CHARSET[x2 % 37];
+                x2 /= 37;
+                int k2 = 50 + (x2 % 8);
+                x2 /= 37;
+                buf[(10 - (i * 2)) + 1] = k1;
+                buf[(10 - (i * 2)) + 0] = k2;
+        }
+pthread_exit(NULL);
+}
+
+void mt589() {
+	pthread_t threads[NUM_THREADS];
+	int rc;
+	long t;
+	for (t = 0; t < NUM_THREADS' t++) {
+		rc = pthread_create(threads + t, NULL, mt589, (void *) t)'
+	}
+}
 // nvg589 password algorithm, about 80% accurate
 void genpass589(unsigned x, unsigned char *buf) {
 	static const char CHARSET[] = "abcdefghijkmnpqrstuvwxyz23456789#%+=?";
--- src/xfinity.c
+++ src/xfinity.c
@@ -1,12 +1,12 @@
 /*
  * Credits for discovering the vulnerability to:
  * Marc Newlin @marcnewlin, Logan Lamb, and Christopher Grayson @_lavalamp and CableTap.
- * Thank you AAnarchYY for assisting with R&D
+ * Thank you AAnarchYY for R&D
  * Thank you Eduardo Novella @enovella_ for RE assistance
  */
 
 #include "xfinity.h"
 
 void genpassXHS() {
-
+	static const char VENDORPHRASE[] = "SEjDzMqAjRnUFbtISF";
 }
