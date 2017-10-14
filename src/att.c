d13df319809ee5b50804958321ff24196e68aa16
soxrok2212 <soxrok2212@gmail.com>
2017-10-14 13:56:01 -0400

    fix nvg599 code and clean nvg 589

--- src/att.c
+++ src/att.c
@@ -1,17 +1,22 @@
 /*
- * Thank you to mrfancypants for research and preliminary Python code for ATTXXXXXXX networks.
- */
-#include <math.h>
+* Thank you to mrfancypants for research and preliminary Python code for ATTXXXXXXX networks.
+*/
+
+#define PW_LENGTH 13
 #include <stdint.h>
 
 #include "att.h"
 
+int i;
+uint64_t fen; // FiveEightNine
+uint64_t fent; // FiveEightNine2
+uint64_t fnn; // FiveNineNine
+
 // nvg589 password algorithm, about 80% accurate
 void genpass589(unsigned x, unsigned char *buf) {
 	static const char CHARSET[] = "abcdefghijkmnpqrstuvwxyz23456789#%+=?";
-	uint64_t x1 = x * 465661287.5245797; // thank you mrfancypants for finding this number
-	uint64_t x2 = x1;
-	int i;
+	fen = x * 465661287.5245797; // thank you mrfancypants for finding this number
+	fent = fen;
 	buf[12] = 0; // create buffer for password
 
 	/*
@@ -20,34 +25,30 @@ void genpass589(unsigned x, unsigned char *buf) {
 	 * possibility
 	 */
 	for (i = 0; i < 6; i++) {
-		int k1 = CHARSET[x2 % 37];
-		x2 /= 37;
-		int k2 = 50 + (x2 % 8);
-		x2 /= 37;
+		int k1 = CHARSET[fent % 37];
+		// calcualate one character of key
+		fent /= 37;
+		int k2 = 50 + (fent % 8);
+		// calculate next character of key
+		fent /= 37;
 		buf[(10 - (i * 2)) + 1] = k1;
 		buf[(10 - (i * 2)) + 0] = k2;
 	}
 }
 
-// nvg599 password algorithm, not finished
-void genpass599(unsigned y, unsigned char *buf) {
+// nvg599 password algorithm
+void genpass599(unsigned x, unsigned char *buf) {
 	static const char CHARSET[] = "abcdefghijkmnpqrstuvwxyz23456789#%+=?";
-	uint64_t y1 = y * pow(2, 32) + 2; // thank you mrfancypants for finding this number
-	uint64_t y2 = y1;
-	int i;
 	buf[12] = 0; // create buffer for password
 
 	/*
-	 * This code finds the associated ascii value in the charset specified above
-	 * If we knew the seed they were using, we wouldn't have to bruteforce every
-	 * possibility
-	 */
-	for (i = 0; i < 6; i++) {
-		int k1 = CHARSET[y2 % 37];
-		y2 /= 37;
-		int k2 = 50 + (y2 % 8);
-		y2 /= 37;
-		buf[(10 - (i * 2)) + 1] = k1;
-		buf[(10 - (i * 2)) + 0] = k2;
+	* This code finds the associated ascii value in the charset specified above
+	* If we knew the seed they were using, we wouldn't have to bruteforce every
+	* possibility
+	*/
+
+	fnn = (double) (x * ((1l << 32) + 2));
+	for (i = 1; i < PW_LENGTH; i++, fnn /= 37) {
+		buf[PW_LENGTH - i - 1] = CHARSET[fnn % 37];
 	}
 }
